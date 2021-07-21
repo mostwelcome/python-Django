e974ab1969b22fa77e5e1d9baf6f550e3cb0df84
mostwelcome <tuadutta13@gmail.com>
2021-07-22 00:07:04 +0530

    added bootstarp

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from blog import urls as blog_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls", namespace='blog'))
+    path("", include(blog_urls, namespace='blog'))
 ]
 
 if settings.DEBUG:
