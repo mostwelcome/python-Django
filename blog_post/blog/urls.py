0d58bb861e44412ecdd2d545e13fed0859ef6f49
mostwelcome <tuadutta13@gmail.com>
2021-08-25 23:39:18 +0530

    added single post

--- blog_post/blog/urls.py
+++ blog_post/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path("", views.starting_page, name='starting-page'),
     path("posts", views.posts, name='post-page'),
     path("posts/<slug>", views.post_details, name='single-post')
-}
+]
