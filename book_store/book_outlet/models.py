891ea7bf4b4282d4b7a6d0228d5b2c3f5fb80832
mostwelcome <tuadutta13@gmail.com>
2021-08-30 09:41:24 +0530

    added relationships

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -3,9 +3,40 @@ from django.db import models
 from django.urls import reverse
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name_plural = "Country Entries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.street}'
+
+    class Meta:
+        # Custom class settings
+        verbose_name_plural = "Address Entries"
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.full_name()
 
 
 class Book(models.Model):
@@ -14,6 +45,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="book")
     is_best_selling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, blank=True)  # Harry Potter 1 -> harry-potter-1
+    published_counties = models.ManyToManyField(Country)
 
     def __str__(self):
         return f'{self.title} {self.rating}'
